4439e433acc5527db620dc4a93ed05a2c599bea8
Ratchaneekorn Suwanwong <r.suwanwong@gmail.com>
2019-01-26 21:57:44 +0700

    no message

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * fastshell custom by janejane
+ * Fiercely quick and opinionated front-ends
+ * https://www.61sec.com
+ * @author Janejane Suwanwong
+ * @version 1.0.1
+ * Copyright 2019. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
